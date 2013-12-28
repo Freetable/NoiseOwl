12b611e22bd6ff6231a9836ebab3f25d58e92cc4
Stephanie Sunshine <stephanie@nirvanadesktop.com>
2013-12-28 04:10:41 -0800

    adding create playlist function

--- assets/js/no-common.js
+++ assets/js/no-common.js
@@ -1,7 +1,19 @@
 
 //<tr><td><button class='btn btn-block btn-info btn-xs'>test</button></td></tr>
 
+window.onresize = function(event) {
+resizeDiv();
+}
+
+function resizeDiv() {
+vpw = $(window).width();
+vph = $(window).height();
+$('#no_queue').css({'height': vph * 0.7});
+$('#no_songs').css({'height': vph * 0.7});
+$('#no_playlists').css({'height': vph * 0.7});
+
 $(document).ready(function() {
+	resizeDiv();
 	update_nickname();
 	get_playlists();
 });
