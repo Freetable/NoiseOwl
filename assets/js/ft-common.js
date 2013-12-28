85a978686988613fd9bb9ba2857b51f838bf2678
Stephanie Sunshine <stephanie@nirvanadesktop.com>
2013-12-27 21:24:56 -0800

    blah

--- assets/js/ft-common.js
+++ assets/js/ft-common.js
@@ -1,15 +1,34 @@
 var nickname = '';
+var playlists = [];
 
-
+function update_nickname() {
 $.ajax({
     type: "GET",
     url: NETWORK_SERVICES_API+"query_user.pls",
     dataType: "json",
-		data: { wwuserid: wwuserid() },
+    data: { wwuserid: wwuserid() },
     success: function(data) { 
 				data = data.shift();
 				console.log(data);
 				nickname = data['nickname'];
 		}
 });
+}
 
+function get_playlists() {
+$.ajax({
+    type: "GET",
+    url: NETWORK_SERVICES_API+"get_playlists.pls",
+    dataType: "json",
+    data: { wwuserid: wwuserid(), sessionid: sessionid() },
+    success: function(data) {
+    	//data = data.shift();
+    	console.log(data);
+    }
+});
+
+
+}
+
+update_nickname();
+get_playlists();
